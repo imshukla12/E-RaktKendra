65f071be397172702c97d5aa1b4caf66e5e5d88a
imshukla12 <shuklaakanksha1298@gmail.com>
2023-04-29 15:56:34 +0530

    user dashboard done

--- frontend/eraktkendra/src/App.js
+++ frontend/eraktkendra/src/App.js
@@ -5,23 +5,25 @@ import HomePage from './components/HomePage'
 import FieldWorkerLogin from './FieldWorkerComponents/FieldWorkerLogin'
 import FieldWorkerDashboard from './FieldWorkerComponents/FieldWorkerDashboard'
 import UserLogin from './UserComponents/UserLogin'
+import UserRegistration from './UserComponents/UserRegistration'
 import UserDashboard from './UserComponents/UserDashboard'
 
 const App = () => {
   return (
-    <div>
+    <div className='font-serif'>
       <BrowserRouter>
       <Routes>
         <Route path='/' element = {<HomePage/>} />
 
         {/* User */}
-        <Route path='/user' element = {<UserDashboard/>} />
         <Route path='/userLogin' element = {<UserLogin/>} />
-
+        <Route path = '/register' element = {<UserRegistration/>} />
+        <Route path='/user' element = {<UserDashboard/>} />
+        
         {/* FieldWorker */}
-        <Route path='/fieldWorker' element = {<FieldWorkerDashboard/>} />
         <Route path='/fieldWorkerLogin' element = {<FieldWorkerLogin/>} />
-
+        <Route path='/fieldWorker' element = {<FieldWorkerDashboard/>} />
+       
       </Routes>
       </BrowserRouter>
     </div>
--- frontend/eraktkendra/src/UserComponents/UserDashboard.js
+++ frontend/eraktkendra/src/UserComponents/UserDashboard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import UserNavbar from './UserNavbar'
+import UserRequests from './UserRequests'
 
 const UserDashboard = () => {
   return (
-    <div>
+    <div className='items-center flex flex-col justify-center'>
         <UserNavbar/>
-        <h1>UserDashboard</h1>
+        <div className='w-4/5 flex justify-center items-center'>
+          <UserRequests/>
+        </div>
     </div>
   )
 }
